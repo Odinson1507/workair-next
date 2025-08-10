ce86cc5d40904055bfdc4f42f26fb1db143f3968
Odinson1507 <gladxon@outlook.com>
2025-08-10 23:34:56 +0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>WorkAir</h1>
+      <p>Welcome. Go to <a href="/chart-of-accounts">Chart of Accounts</a>.</p>
+    </main>
+  );
+}
